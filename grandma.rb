6c446426340e40c9363d409e97961a1ca288f3a5
Nathan Bell <nathanbell09@gmail.com>
2018-11-05 20:36:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
   elsif phrase == phrase.downcase
     "HUH?! SPEAK UP, SUNNY!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 end
 
