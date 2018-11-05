7eb9dc134268c0502362327ba9ed33f7a3a99b42
Nathan Bell <nathanbell09@gmail.com>
2018-11-05 20:40:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase != phrase.upcase
     "HUH?! SPEAK UP, SUNNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
